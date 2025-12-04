a7e532cd99f845c8d2b04a68fb90ec30b705b8bd
Xitlalic Guadalupe Flores Salcedo <128668402+xitflor41@users.noreply.github.com>
2025-12-04 16:02:18 -0600

    Refactor and add unit tests for sumar and promediar
    Updated unit tests for 'sumar' and added tests for 'promediar'.

--- test_operaciones.py
+++ test_operaciones.py
@@ -1,29 +1,20 @@
+# Pruebas unitarias de la funcion "sumar"
 import pytest
 from operaciones import *
 
-#Pruebas unitarias de la funcion "sumar"
+# Prueba para la funcion sumar
 def test_sumar1():
-    res=sumar([1, 2, 3]) 
-    assert res== 6
+    res = sumar([1,2,3])
+    assert res == 6
 
-def test_sumar2():  
-    res=sumar([10, 15, 20]) 
-    assert res== 45
+def test_sumar2():
+    res = sumar([10,15,20])
+    assert res == 45
 
 def test_sumar3():
-    res=sumar([]) 
-    assert res== 0
+    res = sumar([])
+    assert res == 0
 
-#Pruebas unitarias de la funcion "promediar"
-def test_promediar1():
-    res=promediar([1, 2, 3]) 
-    assert res== 2
-
-def test_promediar2():
-    res=promediar([10, 15, 25]) 
-    assert res== 16.666666666666668
-
-def test_promediar3():
-    res=promediar([]) 
-    assert res== 0
-
+def test_promedio1():
+    res = promediar([10,10,7])
+    assert res == 9
